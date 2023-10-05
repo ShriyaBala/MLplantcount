80bd18cc63514cc47a5edce8c25a4c1f877fe7f1
ShriyaBala <shriyabminnu@gmail.com>
2023-10-05 20:54:02 +0530

    done

--- cv_functions.py
+++ cv_functions.py
@@ -1,34 +1,56 @@
 import streamlit as st
 import cv2
 import numpy as np
-from global_functions import ensureDir
 
-# Define your image processing function
-def plant_count_main(img):
-    # Your image processing code here
-    # Make sure to return the result of the processing
-    return 42  # Placeholder result for testing
+# Define constants for image processing
+MIN_GREEN_HUE = 45
+MAX_GREEN_HUE = 77
+MIN_GREEN_SAT = 19
+MAX_GREEN_SAT = 255
+MIN_GREEN_VAL = 164
+MAX_GREEN_VAL = 255
 
-# Define the Streamlit app
+def process_image(img):
+    # Convert the image to the HSV color space
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Define the lower and upper bounds for green color
+    lower_green = np.array([MIN_GREEN_HUE, MIN_GREEN_SAT, MIN_GREEN_VAL])
+    upper_green = np.array([MAX_GREEN_HUE, MAX_GREEN_SAT, MAX_GREEN_VAL])
+
+    # Create a mask to isolate the green regions
+    mask = cv2.inRange(hsv, lower_green, upper_green)
+
+    # Apply morphological operations to the mask to clean up noise
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv2.erode(mask, kernel, iterations=2)
+    mask = cv2.dilate(mask, kernel, iterations=2)
+
+    # Find contours in the mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Count the number of contours (plants)
+    plant_count = len(contours)
+
+    # Draw the contours on the original image
+    img_with_contours = img.copy()
+    cv2.drawContours(img_with_contours, contours, -1, (0, 255, 0), 2)
+
+    return img_with_contours, plant_count
+
 def main():
     st.title("Plant Counting App")
 
-    # Add a file upload widget to let the user upload an image
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
     if uploaded_file is not None:
-        img = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+        img = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
 
-        # Display the source image
         st.image(img, caption="Uploaded Image", use_column_width=True)
 
-        # Add a button to start processing
         if st.button("Process Image"):
-            plants_count = plant_count_main(img)  # Call your image processing function here
+            processed_img, plant_count = process_image(img)
+            st.image(processed_img, caption=f"Processed Image (Plants: {plant_count})", use_column_width=True)
 
-            # Display the count or result
-            st.write(f"Plant Count: {plants_count}")
-
-# You can add the Streamlit app entry point at the end of your script
 if __name__ == "__main__":
     main()
